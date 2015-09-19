21133721daeed0ceb20f93f270298b234f1619b1
sunoru <sunoru@live.com>
2015-09-19 18:03:16 +0800

    small bug

--- chembako/helpers/packmol_helper.py
+++ chembako/helpers/packmol_helper.py
@@ -17,18 +17,18 @@ class Packmol(CommandSet):
         return os.path.join(Config.PACKMOL_BIN_DIR, 'ppackmol' if self.parallel else 'packmol')
 
     def run(self, inp_str):
-        sp = subprocess.Popen([self._packmol_bin], stdin=subprocess.PIPE, stdout=sys.stdout, stderr=self._log_file)
-        self._log("Running packmol with pid: %s." % sp.pid)
+        sp = subprocess.Popen([self._packmol_bin], stdin=subprocess.PIPE, stdout=self._log_file, stderr=self._log_file)
+        self._log_screen("Running packmol with pid: %s." % sp.pid)
         sp.stdin.write(inp_str)
         sp.stdin.close()
         sp.wait()
-        self._log("Packmol completed.")
+        self._log_screen("Packmol completed.")
 
     def run_with_file(self, inp_file):
         if isinstance(inp_file, str):
             inp_file = open(inp_file)
         sp = subprocess.Popen([self._packmol_bin], stdin=inp_file, stdout=self._log_file)
-        self._log("Running packmol with pid: %s." % sp.pid)
+        self._log_screen("Running packmol with pid: %s." % sp.pid)
         sp.wait()
         inp_file.close()
-        self._log("Packmol completed.")
+        self._log_screen("Packmol completed.")
