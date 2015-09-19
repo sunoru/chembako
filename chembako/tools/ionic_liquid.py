fa97cac7ec218d2a1ad3a45fabeb5e09ccfe10a0
sunoru <sunoru@live.com>
2015-09-19 15:54:33 +0800

    debugging

--- chembako/tools/ionic_liquid.py
+++ chembako/tools/ionic_liquid.py
@@ -18,36 +18,36 @@ def packmol_impurity(cation, anion, impurity='', output='packmol.pdb', filetype=
     if seed is None:
         seed = int(time.time() * 1000)
     try:
-        inp_str = 'tolerance 2.0\n'
-        'filetype {filetype}\n'
-        'output {output}\n'
-        'seed {seed}\n'
-        'add_box_sides\n'
-        '\n'
-        'structure {cation}\n'
-        'number {number_il}\n'
-        'inside box 0. 0. 0. {size[0]} {size[1]} {size[2]}\n'
-        'end structure\n'
-        '\n'
-        'structure {anion}\n'
-        'number {number_il}\n'
-        'inside box 0. 0. 0. {size[0]} {size[1]} {size[2]}\n'
-        'end structure\n'
-        '\n'
-        'structure {impurity}\n'
-        'number {number_im}\n'
-        'inside box 0. 0. 0. {size[0]} {size[1]} {size[2]}\n'
-        'end structure\n'.format(
-            filetype=filetype,
-            output=output,
-            seed=seed,
-            cation=cation,
-            anion=anion,
-            impurity=impurity,
-            number_il=number_il,
-            number_im=number_im,
-            size=box_size
-        )
+        inp_str = 'tolerance 2.0\n'\
+            'filetype {filetype}\n'\
+            'output {output}\n'\
+            'seed {seed}\n'\
+            'add_box_sides\n'\
+            '\n'\
+            'structure {cation}\n'\
+            'number {number_il}\n'\
+            'inside box 0. 0. 0. {size[0]} {size[1]} {size[2]}\n'\
+            'end structure\n'\
+            '\n'\
+            'structure {anion}\n'\
+            'number {number_il}\n'\
+            'inside box 0. 0. 0. {size[0]} {size[1]} {size[2]}\n'\
+            'end structure\n'\
+            '\n'\
+            'structure {impurity}\n'\
+            'number {number_im}\n'\
+            'inside box 0. 0. 0. {size[0]} {size[1]} {size[2]}\n'\
+            'end structure\n'.format(
+                filetype=filetype,
+                output=output,
+                seed=seed,
+                cation=cation,
+                anion=anion,
+                impurity=impurity,
+                number_il=number_il,
+                number_im=number_im,
+                size=box_size
+            )
     except TypeError or IndexError or KeyError as e:
         raise e
     packmol_bin = os.path.join(Config.PACKMOL_BIN_DIR, 'ppackmol' if parallel else 'packmol')
